b95b100f7ef2b84b0fec302fb922ccfbd72606e1
iapciinventario-code <iapciinventario@gmail.com>
2026-08-30 22:26:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 // --- CONFIGURACIÓN DE FIREBASE ---
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-  getFirestore, 
-  collection, 
-  getDocs, 
-  addDoc, 
-  updateDoc, 
-  deleteDoc, 
-  doc, 
-  writeBatch, 
-  setDoc, 
-  getDoc 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // 👈 Inicializa la base de datos globalmente
+const analytics = getAnalytics(app);
 // ==========================================
 // CEREBRO UNIFICADO DEL SISTEMA - IAPCI 2026
 // (Sincronización Firestore en Tiempo Real + Respaldo Local + Sesión Única)
